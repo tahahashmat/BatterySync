85f3e421c023c385876d754223197436561a785c
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2020-07-18 01:02:07 +0500

    testing backend branch

--- backEnd/main.js
+++ backEnd/main.js
@@ -11,3 +11,4 @@ child_process.exec("wmic Path Win32_Battery",
             function execBatteryCommand(err, stdout, stderr) {
                 console.log(stdout);
 });
+console.log("big tiddy goth gfs");
