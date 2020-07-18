361964e2295ed781f1f774bd7284d88601dbddd5
Waleed Meraj Qureshi <waleedmerajqureshi/@gmail.com>
2020-07-18 05:57:49 +0300

    Added Model and Manufacturer functions

--- backEnd/main.js
+++ backEnd/main.js
@@ -2,6 +2,8 @@ var child_process = require('child_process');
 
 var osName = "Unknown";
 var batteryPercent = null;
+var modelName = "Unknown";
+var manufacturerName = "Unknown";
 
 switch (process.platform) {
     case "win32":   osName = "Windows"; break;
@@ -12,18 +14,35 @@ switch (process.platform) {
 
 if (osName == "Windows") {
     batteryPercent = getWindowsBattery();
+    manufacturerName = getWindowsManufacturer();
+    modelName = getWindowsModel();
 } else if (osName == "MacOS") {
     batteryPercent = getMacOSBattery();
+    manufacturerName = "Apple";
+    modelName = getMacOSModel();
 } else {
     console.log("Waleed gay");
 }
-
 
 function getWindowsBattery() {
     var data = child_process.execSync("wmic Path Win32_Battery get estimatedchargeremaining").toString();
     var batteryPercent = data.substring(29);
 
-    return batteryPercent;
+    return batteryPercent.trim();
+}
+
+function getWindowsManufacturer(){
+    var data = child_process.execSync("wmic computersystem get manufacturer").toString();
+    var manufacturer = data.substring(12);
+
+    return manufacturer.trim(); 
+}
+
+function getWindowsModel(){
+    data = child_process.execSync("wmic computersystem get model").toString();
+    var model = data.substring(5);
+
+    return model.trim();    
 }
 
 function getMacOSBattery() {
@@ -34,5 +53,13 @@ function getMacOSBattery() {
     return batteryPercent;
 }
 
-console.log("OS Name: " + osName + "\nBattery Percentage: " + batteryPercent);
+function getMacOSModel() {
+    var data = child_process.execSync('sysctl hw.model').toString();
+    var arrayData = (data.substring(10)).split(",");
+    modelName = arrayData[0];
+
+    return modelName;
+}
+
+console.log("OS Name: " + osName + "\nBattery Percentage: " + batteryPercent + "\nManufacturer: " + manufacturerName + "\nModel: " + modelName);
 
