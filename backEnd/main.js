9e6ebae779829534718720f139a88ef3dec104af
Abdullah Riaz <abdullah.raja@ontariotechu.net>
2020-07-17 21:14:56 -0400

    Added Battery function for MacOS
    Still testing

--- backEnd/main.js
+++ backEnd/main.js
@@ -1,23 +1,38 @@
 var child_process = require('child_process');
 
-var osName;
+var osName = "Unknown";
+var batteryPercent = null;
 
 switch (process.platform) {
-    case "win32":
-        osName = "Windows";
-        break;
-    case "darvin":
-        osName = "MacOS";
-        break;
-    case "linux":
-        osName = "Linux";
-        break;
-    default:
-        break;
+    case "win32":   osName = "Windows"; break;
+    case "darvin":  osName = "MacOS";   break;
+    case "linux":   osName = "Linux";   break;
+    default: break;
 }
 
-var data = child_process.execSync("wmic Path Win32_Battery get estimatedchargeremaining").toString();
-var batteryPercent = data.substring(29);
+if (osName == "Windows") {
+    batteryPercent = getWindowsBattery();
+} else if (osName == "MacOs") {
+    batteryPercent = getMacOSBattery();
+} else {
+    console.log("Waleed gay");
+}
+
+
+function getWindowsBattery() {
+    var data = child_process.execSync("wmic Path Win32_Battery get estimatedchargeremaining").toString();
+    var batteryPercent = data.substring(29);
+
+    return batteryPercent;
+}
+
+function getMacOSBattery() {
+    var data = child_process.execSync('pmset -g batt | egrep "([0-9]+%).*" -o').toString();
+    var arrayData = data.split("%");
+    batteryPercent = arrayData[0];
+
+    return batteryPercent;
+}
 
 console.log("OS Name: " + osName + "\nBattery Percentage: " + batteryPercent);
 
