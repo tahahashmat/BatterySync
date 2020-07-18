dd8ade706188c63b81b774c2a9e10579a094f803
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2020-07-18 13:46:34 +0500

    Added compatibility for devices which have no batteries

--- backEnd/main.js
+++ backEnd/main.js
@@ -3,28 +3,66 @@ var child_process = require('child_process');
 const fetch = require("node-fetch"); // this is for calling api
 
 
+var batteryFlag;
 var osName = "Unknown";
 var batteryPercent = null;
 var modelName = "Unknown";
 var manufacturerName = "Unknown";
 
 switch (process.platform) {
-    case "win32":   osName = "Windows"; break;
-    case "darwin":  osName = "MacOS";   break;
-    case "linux":   osName = "Linux";   break;
-    default: break;
+    case "win32":
+      osName = "Windows";
+      batteryFlag = hasBattery(osName);
+      break;
+    case "darwin":
+      osName = "MacOS";
+      batteryFlag = hasBattery(osName);
+      break;
+    case "linux":
+      osName = "Linux";
+      batteryFlag = hasBattery(osName);
+      break;
+    default:
+      break;
 }
 
 if (osName == "Windows") {
-    batteryPercent = getWindowsBattery();
+    if(batteryFlag == true){
+      batteryPercent = getWindowsBattery();
+    }
     manufacturerName = getWindowsManufacturer();
     modelName = getWindowsModel();
 } else if (osName == "MacOS") {
-    batteryPercent = getMacOSBattery();
+    if(batteryFlag == true){
+      batteryPercent = getMacOSBattery();
+    }
     manufacturerName = "Apple";
     modelName = getMacOSModel();
 } else {
-    console.log("Waleed gay");
+    console.log("Waleed big gay and have small pp");
+}
+
+//check if device has battery, false means no battery
+function hasBattery(currentOS){
+    var data;
+    if(currentOS == "Windows"){
+      data = child_process.execSync("wmic Path Win32_Battery get estimatedchargeremaining").toString();
+      if (typeof(data) == "undefined"){
+        return false;
+      }
+      else{
+        return false;
+      }
+    }
+    else if(currentOS == "MacOS"){
+      data = child_process.execSync('pmset -g batt | egrep "([0-9]+%).*" -o').toString();
+      if (typeof(data) == "undefined"){
+        return false;
+      }
+      else{
+        return false;
+      }
+    }
 }
 
 function getWindowsBattery() {
@@ -38,14 +76,14 @@ function getWindowsManufacturer(){
     var data = child_process.execSync("wmic computersystem get manufacturer").toString();
     var manufacturer = data.substring(12);
 
-    return manufacturer.trim(); 
+    return manufacturer.trim();
 }
 
 function getWindowsModel(){
-    data = child_process.execSync("wmic computersystem get model").toString();
+    var data = child_process.execSync("wmic computersystem get model").toString();
     var model = data.substring(5);
 
-    return model.trim();    
+    return model.trim();
 }
 
 function getMacOSBattery() {
@@ -84,5 +122,3 @@ fetch('https://us-central1-batterysync-89680.cloudfunctions.net/api/updateBatter
 }).catch((thing)=> {
     console.log(thing)
 })
-
-
