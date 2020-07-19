6657c8a4a31a95f3c65941285641860afba629e3
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2020-07-19 09:25:21 +0500

    minor changes

--- backEnd/BatterySyncDesktopClient/src/script.js
+++ backEnd/BatterySyncDesktopClient/src/script.js
@@ -2,15 +2,15 @@ var username;
 var password;
 
 document.getElementById("loginButton").addEventListener("click", function(){
+  console.log("LOCAL STORAGE :")
+  console.log(localStorage.getItem('USERNAME'));
+  console.log(localStorage.getItem('PASSWORD'));
   username = getUsername();
   password = getPassword();
   console.log("Username: " + username);
   console.log("Password: " + password);
   localStorage.setItem('USERNAME', username);
   localStorage.setItem('PASSWORD', password);
-  console.log("LOCAL STORAGE :")
-  console.log(localStorage.getItem('USERNAME'));
-  console.log(localStorage.getItem('PASSWORD'));
 });
 
 function getUsername(){
