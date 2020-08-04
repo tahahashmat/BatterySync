cd4162d3b7dc1b69f50fa50915ce14bbb91919d4
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2020-08-04 12:51:53 +0500

    added authentication to the client app

--- backEnd/BatterySyncDesktopClient/src/loginPage.js
+++ backEnd/BatterySyncDesktopClient/src/loginPage.js
@@ -1,14 +1,32 @@
 const electron = require('electron');
 const ipc = electron.ipcRenderer
 
+//firebase shit
+var firebase = require("firebase/app");
+const { registerVersion } = require('firebase/app');
+
+require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAzqaEF4Oo_3d35UvSM7HvU7D7tiFj1-yc",
+  authDomain: "batterysync-89680.firebaseapp.com",
+  databaseURL: "https://batterysync-89680.firebaseio.com",
+  projectId: "batterysync-89680",
+  storageBucket: "batterysync-89680.appspot.com",
+  messagingSenderId: "492042714028",
+  appId: "1:492042714028:web:36ec5ada767c69d5781809",
+  measurementId: "G-VSK0JQ4WN1"
+};
+
+firebase.initializeApp(firebaseConfig);
+//end
+
 var email;
 var password;
 
 document.getElementById("loginButton").addEventListener("click", login);
 document.getElementById("loginButton").addEventListener("click", storeLoginDetailToLocalStorage);
-// document.getElementById("loginButton").addEventListener("click", function(){
-//   ipc.send('open-error-dialog')
-// });
 
 function getEmail(){
   return document.getElementById('email').value;
@@ -33,38 +51,24 @@ function storeLoginDetailToLocalStorage(){
 function login(){
   email = getEmail();
   password = getPassword();
-  if(email == "1" && password == "1"){
-    //firebase shit
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((result) => {
+    console.log("Signed in!");
     showSettingsWindow();
     console.log("Login succesful")
-  }
-  else{
-    console.log("Wrong email or password");
-    if(document.getElementById("error-text") == null){
-      var para = document.createElement("p");
-      para.setAttribute("id", "error-text");
-      var node = document.createTextNode("Wrong Email or Password!");
-      para.appendChild(node);
-      var element = document.getElementById("login-button-div")
-      var child = document.getElementById("loginButton");
-      element.insertBefore(para, child);
-    }
-  }
+  }).catch((error) => {
+    var para = document.createElement("p");
+    para.setAttribute("id", "error-text");
+    var node = document.createTextNode(error);
+    para.appendChild(node);
+    var element = document.getElementById("login-button-div")
+    var child = document.getElementById("loginButton");
+    element.insertBefore(para, child);
+  });
+
 }
 
 function showSettingsWindow(){
   ipc.send('show-settings-window')
-} 
-
-//Settings window 
-// var settingsWindow = new BrowserWindow({
-//   width: 200,
-//   height: 200,
-//   show: false
-// });
-
-// settingsWindow.loadFile('settings.html');
-
-// ipc.on('show-settingsWindow', function(){
-//   settingsWindow.show();
-// })
+}
