e25b69928ada1efa1b704e6a65efdc06a8e7af37
Mujtaba Ahmed <m.mujtabaahmed2000@gmail.com>
2020-08-13 22:34:03 +0500

    syncs battery info every 20 seconds

--- backEnd/BatterySyncDesktopClient/src/settingsPage.js
+++ backEnd/BatterySyncDesktopClient/src/settingsPage.js
@@ -4,14 +4,14 @@ const child_process = require('child_process');
 const batteryReader = require('./main')
 
 var email;
+var interval
 
 document.getElementById("start-syncing-button").addEventListener("click", function(){
-    console.log("Button pressed")
     startSyncing()
 })
 
 document.getElementById("stop-syncing-button").addEventListener("click", function(){
-    console.log("Button pressed")
+    stopSyncing()
 })
 
 document.getElementById("logout-button").addEventListener("click", function(){
@@ -27,12 +27,20 @@ function showLoginPage(){
 
 function startSyncing(){
     //CHECK BATTERY FIRST AND SHOW ERROR BOX
+    document.getElementById("start-syncing-button").disabled = true;
+    document.getElementById("stop-syncing-button").disabled = false;
     email = getEmailLocalStorage()
-    batteryReader.sendBatteryInfo(true, email)
+    interval = setInterval(batteryReader.sendBatteryInfo(true, email), '20000')
+    var status = document.getElementById('status')
+    status.style.color = 'green'
 }
 
 function stopSyncing(){
-
+    document.getElementById("start-syncing-button").disabled = false;
+    document.getElementById("stop-syncing-button").disabled = true;
+    clearInterval(interval)
+    var status = document.getElementById('status')
+    status.style.color = 'red'
 }
 
 //Checks if the current machine has a battery, if false will show error and not send battery information
